1b1bd3bbb72d0a750393b931a6dd20267dfa65c7
Linear Music <amaannn.gupta.08@gmail.com>
2025-03-26 02:31:59 +0530

    made changes to vite.config.js

--- Frontend/lic-app/vite.config.js
+++ Frontend/lic-app/vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  build: {
+    outDir: 'dist'
   },
-})
+  server: {
+    historyApiFallback: true
+  }
+});
